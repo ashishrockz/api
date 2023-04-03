2260d3e78c94eac5a8c5ba5ff352028bc041f036
B CHUCHEL ASHISH <85539405+ashishrockz@users.noreply.github.com>
2023-04-03 15:11:15 +0530

    update

--- server/index.js
+++ server/index.js
@@ -11,7 +11,13 @@ connection();
 // middlewares
 app.use(express.json());
 app.use(cors());
-
+//
+if (process . env.NODE_ENV === "production "){
+    app.use(express. static (path . join ("frontend/build ")));
+    app.get("*", (req, res) => {
+        res.sendFile (path. resolve (_dirname, "frontend", "build", "index.html"));
+    });
+}
 // routes
 
 const contentsRouter = require("./router/contents");
